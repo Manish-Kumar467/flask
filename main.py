9748012dba53af2d43cd528478f3d6fb5b2d57bc
Manish K <126048493+Manish-Kumar467@users.noreply.github.com>
2024-09-29 12:57:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -8,3 +9,17 @@ def index():
 
 if __name__ == '__main__':
   app.run(port=5000)
+
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PORT = os.getenv('DB_PORT')
+
+conn = psycopg2.connect(
+    host=DB_HOST,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    port=DB_PORT
+)
