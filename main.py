4d7fca5936bab596f1c359bd340f524ce1319974
Manish-Kumar467 <mkwork467@gmail.com>
2024-11-12 17:05:15 +0530

    changed main to initial

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-from flask import Flask, request, jsonify
-import joblib
-import numpy as np
-import pandas as pd #
-import pickle #
-from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)  # Allow CORS for all routes
 
-# Load the SVC model
-model = joblib.load('./models/svc.pkl')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        data = request.json  # JSON input
-        symptoms_list = data.get('symptoms', [])
-        
-        if not symptoms_list:
-            return jsonify({'error': 'No symptoms provided'}), 400
-        
-        # Ensure input shape compatibility
-        input_data = np.array([symptoms_dict.get(symptom, 0) for symptom in symptoms_list]).reshape(1, -1)
-        
-        # Predict
-        prediction = model.predict(input_data)
-        predicted_disease = diseases_list[prediction[0]]
-        
-        return jsonify({'disease': predicted_disease})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
-
+  app.run(port=5000)
