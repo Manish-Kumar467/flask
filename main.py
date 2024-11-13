7fe3097e18f4ce1a83dbe912b65c87e8914564df
Manish-Kumar467 <mkwork467@gmail.com>
2024-11-13 22:34:30 +0530

    changed back the procfile and main.py app.run

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000)
+  app.run(port=5000, debug=True)
 
 # from flask import Flask, request, jsonify
 # import joblib
